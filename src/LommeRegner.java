1e262d53fdd20a3b67effb612173fb758a3240a9
yokarak <ande67f3@edu.easj.dk>
2019-09-16 11:44:05 +0200

    Lommeregner projekt tilføjede secret i alle

--- src/LommeRegner.java
+++ src/LommeRegner.java
@@ -12,23 +12,29 @@ public class LommeRegner {
 
         public double minus(double c, double d) {
         double resultat = c - d;
+            if (c==69 && d==69)  {
+                return resultat = 69; } else
         return resultat;
         }
 
         public double multiplikere(double e, double f) {
         double resultat = e * f;
+            if (e==69 && f==69)  {
+                return resultat = 69; } else
         return  resultat;
         }
 
         public double dele(double g, double h) {
         double resultat = g / h;
+            if (g==69 && h==69)  {
+                return resultat = 69; } else
         return resultat;
         }
 /* sat ind i sum
         public double secret(double a, double b) {
         double resultat = a + b;
         if (resultat == 69)  {
-        return resultat = 69^2; }
+        return resultat = 69; }
         }
   */
 }
