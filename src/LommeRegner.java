531785d85180d87a5079ebd21b6219c1dc7eb167
yokarak <ande67f3@edu.easj.dk>
2019-09-16 11:52:02 +0200

    Lommeregner projekt ændrede fra public til protected (laver overvejlser om at lave det til private og set og get)

--- src/LommeRegner.java
+++ src/LommeRegner.java
@@ -1,30 +1,27 @@
-public class LommeRegner {
+ class LommeRegner {
 
-    boolean start;
-
-
-    public double sum(double a, double b) {
+    protected double sum(double a, double b) {
         double resultat = a + b;
         if (a==69 && b==69)  {
             return resultat = 69; } else
         return resultat;
         }
 
-        public double minus(double c, double d) {
+        protected double minus(double c, double d) {
         double resultat = c - d;
             if (c==69 && d==69)  {
                 return resultat = 69; } else
         return resultat;
         }
 
-        public double multiplikere(double e, double f) {
+        protected double multiplikere(double e, double f) {
         double resultat = e * f;
             if (e==69 && f==69)  {
                 return resultat = 69; } else
         return  resultat;
         }
 
-        public double dele(double g, double h) {
+        protected double dele(double g, double h) {
         double resultat = g / h;
             if (g==69 && h==69)  {
                 return resultat = 69; } else
